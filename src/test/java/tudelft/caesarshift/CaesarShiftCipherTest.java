9104ec255a0a2efd502442ecb27bf55e19038807
kinuFedora <kinu@Fedora.home>
2026-01-18 19:11:55 +0500

    Learnt lessons on Coverage - CaesarCipher and CountLetters

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,13 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
 
+  @Test 
+  public void test1() {
+    CaesarShiftCipher cipher = new CaesarShiftCipher();
+    Assertions.assertEquals("abbccd", cipher.encrypt("abc",1));
+  }
 }
--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -6,15 +6,44 @@ import org.junit.jupiter.api.Test;
 public class CountLettersTest {
 
     @Test
-    public void multipleMatchingWords() {
+    public void multipleMatchingWordsR() {
+        int words = new CountLetters().count("eater|rider");
+        Assertions.assertEquals(2, words);
+    }
+
+    @Test
+    public void lastWordDoesNotMatchR() {
+        int words = new CountLetters().count("eater|ride");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void firstWordDoesNotMatchR() {
+        int words = new CountLetters().count("eat|rider");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void multipleMatchingWordsS() {
         int words = new CountLetters().count("cats|dogs");
         Assertions.assertEquals(2, words);
     }
 
     @Test
-    public void lastWordDoesNotMatch() {
+    public void lastWordDoesNotMatchS() {
         int words = new CountLetters().count("cats|dog");
         Assertions.assertEquals(1, words);
     }
 
+    @Test
+    public void firstWordDoesNotMatchS() {
+        int words = new CountLetters().count("cat|dogs");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void noMatchingWords() {
+        int words = new CountLetters().count("cat|dog");
+        Assertions.assertEquals(0, words);
+    }
 }
